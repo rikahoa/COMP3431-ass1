4aa17a31d04778e93da9e3365a2f0fad7fb2d12d
Oliver Tan <sinovercosequalstan@gmail.com>
2015-08-25 20:48:03 +1000

    movement nodes

---
+++ src/ass1/src/beacon_finder.cpp
@@ -0,0 +1,30 @@
+#include "ros/ros.h"
+
+class Beacon {
+    private:
+        double x, y;
+        // COLOURS
+};
+
+class BeaconFinder {
+    public:
+        
+        BeaconFinder(ros::NodeHandle n) : n(n) {
+            // location_pub = n.advertise<??>("/ass1/beacons", 1);
+        }
+
+    private:
+        ros::NodeHandle n;
+        ros::Publisher location_pub;
+};
+
+int main(int argc, char *argv[]) {
+    ros::init(argc, argv, "beacon");
+    ros::NodeHandle n;
+
+    BeaconFinder beacon_finder(n);
+
+    ros::spin();
+
+    return 0;
+}
--- src/ass1/src/exploration.cpp
+++ src/ass1/src/exploration.cpp
@@ -1,18 +1,23 @@
 #include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
 
 class Exploration {
     public:
         
         Exploration(ros::NodeHandle n) : n(n) {
+            movement_pub = n.advertise<geometry_msgs::Twist>("/ass1/movement", 1);
         }
 
     private:
         ros::NodeHandle n;
+        ros::Publisher movement_pub;
 };
 
 int main(int argc, char *argv[]) {
     ros::init(argc, argv, "exploration");
     ros::NodeHandle n;
+
+    Exploration exploration(n);
 
     ros::spin();
 
