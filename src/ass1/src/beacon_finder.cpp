0f698ab049f6e7c3ba9e487349a0c79cff4b2afc
Oliver Tan <sinovercosequalstan@gmail.com>
2015-08-28 00:10:09 +1000

    beacon finder improvements

--- src/ass1/src/beacon_finder.cpp
+++ src/ass1/src/beacon_finder.cpp
@@ -29,55 +29,56 @@ private:
 
 class BeaconFinder {
 public:
-    BeaconFinder(ros::NodeHandle n, vector<Beacon> beacons) : n(n), beacons(beacons) {
+    BeaconFinder(ros::NodeHandle n, vector<Beacon> beacons) : n(n), beacons(beacons), it(n) {
         // location_pub = n.advertise<??>("/ass1/beacons", 1);
+        image_sub = it.subscribe("/camera/rgb/image_color", 1, &BeaconFinder::image_callback, this);
     }
 
 private:
     ros::NodeHandle n;
-    ros::Publisher location_pub;
     vector<Beacon> beacons;
-};
+    image_transport::ImageTransport it;
+	image_transport::Subscriber image_sub;
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
-    try {
-        // Convert from ROS image msg to OpenCV matrix images
-        cv_bridge::CvImagePtr cv_ptr;
-        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-        cv::Mat src = cv_ptr->image;
-        imshow("original", src);
+    void image_callback(const sensor_msgs::ImageConstPtr& msg) {
+        try {
+            // Convert from ROS image msg to OpenCV matrix images
+            cv_bridge::CvImagePtr cv_ptr;
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+            cv::Mat src = cv_ptr->image;
+            imshow("original", src);
 
-        // OpenCV filters to find colours
-        cv::Mat hsv, pink_threshold, yellow_threshold;
-        cv::cvtColor(src, hsv, CV_BGR2HSV);
-        cv::inRange(hsv, cv::Scalar(140,90,90), cv::Scalar(180,255,255), pink_threshold);
-        cv::inRange(hsv, cv::Scalar(20,90,90), cv::Scalar(30,255,255), yellow_threshold);
+            // OpenCV filters to find colours
+            cv::Mat hsv, pink_threshold, yellow_threshold;
+            cv::cvtColor(src, hsv, CV_BGR2HSV);
+            cv::inRange(hsv, cv::Scalar(140,90,90), cv::Scalar(180,255,255), pink_threshold);
+            cv::inRange(hsv, cv::Scalar(20,90,90), cv::Scalar(30,255,255), yellow_threshold);
 
+            // blob detection
+            cv::SimpleBlobDetector::Params params; 
+            params.filterByCircularity = true;
+            params.minCircularity = 0.1;
+            params.filterByArea = true;
+            params.minArea = 100;
+            
+            std::vector<cv::KeyPoint> keypoints;
+            cv::SimpleBlobDetector detector(params);
+            detector.detect(pink_threshold, keypoints);
+            cv::Mat blobs;
+            cv::drawKeypoints( pink_threshold, keypoints, blobs, 
+                    cv::Scalar(0,0,255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
 
-        // blob detection
-        cv::SimpleBlobDetector::Params params; 
-        params.filterByCircularity = true;
-        params.minCircularity = 0.1;
-        params.filterByArea = true;
-        params.minArea = 100;
-        
-        std::vector<cv::KeyPoint> keypoints;
-        cv::SimpleBlobDetector detector(params);
-        detector.detect(pink_threshold, keypoints);
-        cv::Mat blobs;
-        cv::drawKeypoints( pink_threshold, keypoints, blobs, 
-                cv::Scalar(0,0,255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+            // gui display
+            imshow("Blobs", blobs);
+            imshow("Pink", pink_threshold);
+            imshow("Yellow", yellow_threshold);
 
-        // gui display
-        imshow("Blobs", blobs);
-        imshow("Pink", pink_threshold);
-        imshow("Yellow", yellow_threshold);
-
-        cv::waitKey(30);
-    } catch (cv_bridge::Exception& e) {
-        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+            cv::waitKey(30);
+        } catch (cv_bridge::Exception& e) {
+            ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+        }
     }
-}
+};
 
 int main(int argc, char *argv[]) {
     ros::init(argc, argv, "beacon_finder");
@@ -114,9 +115,7 @@ int main(int argc, char *argv[]) {
     cv::startWindowThread();
 
     BeaconFinder beacon_finder(n, beacons);
-
-    image_transport::ImageTransport it(n);
-	image_transport::Subscriber sub = it.subscribe("/camera/rgb/image_color", 1, imageCallback);
+    
     ros::spin();
     cv::destroyWindow("Pink");
     cv::destroyWindow("Yellow");
--- src/ass1/src/movement.cpp
+++ src/ass1/src/movement.cpp
@@ -15,35 +15,41 @@ public:
         message_filters::Subscriber<geometry_msgs::TwistStamped> movement_sub(n, "/ass1/movement", 1);
         message_filters::Subscriber<sensor_msgs::LaserScan> laser_sub(n, "/scan", 1);
         
-        typedef message_filters::sync_policies::ApproximateTime<geometry_msgs::TwistStamped, sensor_msgs::LaserScan> ApproxTwistLaserPolicy;
+        typedef message_filters::sync_policies::
+            ApproximateTime<geometry_msgs::TwistStamped, sensor_msgs::LaserScan> 
+            ApproxTwistLaserPolicy;
 
-        message_filters::Synchronizer<ApproxTwistLaserPolicy> sync(ApproxTwistLaserPolicy(5), movement_sub, laser_sub);
+        message_filters::Synchronizer<ApproxTwistLaserPolicy> 
+            sync(ApproxTwistLaserPolicy(5), movement_sub, laser_sub);
         sync.registerCallback(boost::bind(&Movement::movement_and_laser_callback, this, _1, _2));
     }
 private:
     ros::NodeHandle n;
     ros::Publisher navi_pub;
 
-void movement_and_laser_callback(const geometry_msgs::TwistStampedConstPtr &twistStamped, const sensor_msgs::LaserScanConstPtr &laserScan) {
-    ROS_DEBUG_STREAM("Moving x = " << twistStamped->twist.linear.x << ", angle z = " << twistStamped->twist.angular.z);
+    void movement_and_laser_callback(const geometry_msgs::TwistStampedConstPtr &twistStamped, 
+            const sensor_msgs::LaserScanConstPtr &laserScan) {
+        ROS_DEBUG_STREAM("Moving x = " << twistStamped->twist.linear.x << 
+                ", angle z = " << twistStamped->twist.angular.z);
 
-    bool safe = false;
+        bool safe = false;
 
-    if (twistStamped->twist.angular.z < laserScan->angle_min || twistStamped->twist.angular.z > laserScan->angle_max) {
-        safe = true;
-    } else {
-        double angleDelta = twistStamped->twist.angular.z - laserScan->angle_min;
-        int delta = angleDelta/laserScan->angle_increment;
-        
-        if(laserScan->ranges[delta] < 0.1) {
-            safe = true;            
+        if (twistStamped->twist.angular.z < laserScan->angle_min || 
+                twistStamped->twist.angular.z > laserScan->angle_max) {
+            safe = true;
+        } else {
+            double angle_delta = twistStamped->twist.angular.z - laserScan->angle_min;
+            int delta = angle_delta / laserScan->angle_increment;
+            
+            if (laserScan->ranges[delta] < 0.1) {
+                safe = true;            
+            }
+        }
+
+        if (safe) {
+             navi_pub.publish(twistStamped->twist);
         }
     }
-
-    if (safe) {
-         navi_pub.publish(twistStamped->twist);
-    }
-}
 };
 
 int main(int argc, char *argv[]) {
