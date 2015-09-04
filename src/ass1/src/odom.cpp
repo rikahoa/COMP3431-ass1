caf5e7866790205847bdf9e8aaf9cef6a24bdb4f
Oliver Tan <sinovercosequalstan@gmail.com>
2015-09-04 13:22:22 +1000

    wait for transform fix i think!

--- src/ass1/src/odom.cpp
+++ src/ass1/src/odom.cpp
@@ -18,14 +18,17 @@ int main(int argc, char *argv[]) {
     
     while (n.ok()) {
         try {
+            nav_msgs::Odometry odom;
+            odom.header.frame_id = transform.frame_id_;
+            odom.child_frame_id = transform.child_frame_id_;
+
             ros::Time now = ros::Time::now();
-            listener.waitForTransform("/map", "/base_link", now, ros::Duration(1.0));            
-            listener.lookupTransform("/map", "/base_link", now, transform);
-            
-            nav_msgs::Odometry odom;
-            odom.header.stamp = transform.stamp_;
-            odom.header.frame_id = transform.frame_id_;
-            odom.child_frame_id = transform.child_frame_id_;
+            if (listener.waitForTransform("/map", "/base_link", now, ros::Duration(1.0))) {
+                listener.lookupTransform("/map", "/base_link", now, transform);
+                odom.header.stamp = transform.stamp_;
+            } else {
+                odom.header.stamp = ros::Time::now();
+            }
 
             odom.pose.pose.position.x = transform.getOrigin().x();
             odom.pose.pose.position.y = transform.getOrigin().y();
@@ -34,32 +37,13 @@ int main(int argc, char *argv[]) {
             odom.pose.pose.orientation.y = transform.getRotation().y();
             odom.pose.pose.orientation.z = transform.getRotation().z();
             odom.pose.pose.orientation.w = transform.getRotation().w();
-
+            
             clean_odom_pub.publish(odom);
-        } catch (tf::TransformException ex) {
-            ROS_INFO("SENDING OLD TRANSFORM with new Date");
-            
-            nav_msgs::Odometry odom;
-            odom.header.stamp = ros::Time::now();
-            odom.header.frame_id = transform.frame_id_;
-            odom.child_frame_id = transform.child_frame_id_;
-
-            odom.pose.pose.position.x = transform.getOrigin().x();
-            odom.pose.pose.position.y = transform.getOrigin().y();
-            odom.pose.pose.position.z = 0;
-            odom.pose.pose.orientation.x = transform.getRotation().x();
-            odom.pose.pose.orientation.y = transform.getRotation().y();
-            odom.pose.pose.orientation.z = transform.getRotation().z();
-            odom.pose.pose.orientation.w = transform.getRotation().w();
-
-            clean_odom_pub.publish(odom);
-            
-            
+        } catch (tf::TransformException &ex) {
             ROS_ERROR("Error! %s", ex.what());
         } 
             
         ros::spinOnce();
-
     }
 
     return 0;
