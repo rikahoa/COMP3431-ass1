32a6315211d77f3c5ab4c91ba0147e116fd47cbb
Oliver Tan <sinovercosequalstan@gmail.com>
2015-09-05 20:38:15 +1000

    moved maze functions into cpp

--- src/ass1/src/ass1lib/maze.h
+++ src/ass1/src/ass1lib/maze.h
@@ -27,54 +27,7 @@ public:
         fatten_neighbours(og);
     }
 
-    #define FATTEN 4
-    #define MIN_PROB 80
-    void fatten_neighbours(const nav_msgs::OccupancyGrid &og) {
-        vector<vector<bool>> seen;
-        for (int y = 0; y < og.info.height; ++y) {
-            seen.push_back(vector<bool>(og.info.width));
-        }
-
-        queue<pair<pair<int, int>, int>> bfs;
-        for (int y = 0; y < this->og.info.height; ++y) {
-            for (int x = 0; x < this->og.info.width; ++x) {
-                if (og.data[y * og.info.height + x] >= 50) {
-                    bfs.push(make_pair(make_pair(x, y), 0));
-                }
-            }
-        }
-
-        // run through all possible positions.
-        while (!bfs.empty()) {
-            auto info = bfs.front();
-            bfs.pop();
-            
-            // Find information.
-            auto x = info.first.first;
-            auto y = info.first.second;
-            auto distance = info.second;
-           
-            if (seen[y][x] || distance > FATTEN) {
-                continue;
-            }
-
-            // Mark data
-            seen[y][x] = true;
-            set_data(y, x, 100);
-
-            // search all directions
-            for (const auto& dir : Maze::DIRECTIONS) {
-                auto newX = x + dir.first;
-                auto newY = y + dir.second;
-                if (newX >= 0 && newX < og.info.width && newY >= 0 && newY < og.info.height &&
-                        !seen[newY][newX] && distance < FATTEN) {
-                    bfs.push(make_pair(make_pair(newX, newY), distance + 1));
-                }
-            }
-        }
-    }
-
-    queue<pair<double, double>> og_to_real_path(const vector<pair<int, int>>& astar_path) {
+    queue<pair<double, double>> og_to_real_path(const vector<pair<int, int>>& astar_path) const {
         queue<pair<double, double>> real_path;
         for (const auto &point : astar_path) {
             real_path.push(this->get_world_pos(point));
@@ -83,7 +36,7 @@ public:
     }
 
     // RIP Aneita
-    vector<pair<int, int>> path_to_grid(const vector<pair<int, int>>& astar_path) {
+    vector<pair<int, int>> path_to_grid(const vector<pair<int, int>>& astar_path) const {
         vector<pair<int, int>> grid_path;
 
         for (const auto& a : astar_path) {
@@ -132,6 +85,8 @@ public:
                          static_cast<int>((pos.second - origin.y) / this->og.info.resolution));
     }
 private:
+    void fatten_neighbours(const nav_msgs::OccupancyGrid &og);
+    
     nav_msgs::OccupancyGrid og;   
     static const vector<pair<int, int>> DIRECTIONS;
 };
